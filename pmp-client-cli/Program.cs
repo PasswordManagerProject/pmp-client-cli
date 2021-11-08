26e6d2a44d4b36832f40b888ce367df37846546d
dima <naumenko.dmitriy@gmail.com>
2021-11-07 21:14:53 -0800

    test

---
+++ pmp-client-cli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pmp_client_cli
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
